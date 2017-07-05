74bc8dc7a14225fb0e66ce6329655b64c73c33f9
Steven <shockshocksoul@gmail.com>
2017-07-05 15:13:22 +0800

    Gulp 🍷 rock'n roll

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,33 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect-php');
-	browserSync = require('browser-sync');
- 
-gulp.task('watch', function() {
-  connect.server({}, function (){
-    browserSync({
-      proxy: '127.0.0.1:8000'
+    connect = require('gulp-connect-php'),
+    browserSync = require('browser-sync'),
+    sass = require('gulp-ruby-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    prefix = require('gulp-autoprefixer');
+
+
+gulp.task('sass', function() {
+    sass('resources/sass/app.sass', {
+            sourcemap: true,
+            style: 'compact'
+        })
+        .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('browser-sync', function() {
+    connect.server({}, function() {
+        browserSync({
+            proxy: '127.0.0.1:8000'
+        });
     });
-  });
- 
-  gulp.watch('*.php').on('change', function () {
-    browserSync.reload();
-  });
+
+    gulp.watch('*.php').on('change', function() {
+        browserSync.reload();
+    });
+});
+
+gulp.task('watch', ['sass', 'browser-sync'], function() {
+    gulp.watch("resources/sass/*.sass", ['sass']);
 });
